4fdffe8017d34aea3bd350f6479140af4141a5cc
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-08-07 23:09:49 +0900

    modify: change @comment color

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -8,13 +8,17 @@ return {
       style = "darker",
       transparent = true,
       term_colors = true,
+      code_style = {
+        comments = 'none'
+      },
       highlights = {
         LineNr = { fg = "#ABB2BF" },
+        ["@comment"] = { fg = "#8C95A8" },
       },
     })
 
     onedark.load()
 
-    vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#535965', bg = 'NONE' })
+    vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#535965", bg = "NONE" })
   end,
 }
