7390619af0a5ea3d4d7b6ab56da309cad26645ac
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-08-10 12:37:00 +0900

    feature: auto-open Project search window if nvim is opened in home directory

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -3,9 +3,20 @@ return {
   config = function()
     local auto_session = require "auto-session"
 
+    local no_restore_hook = function()
+      local home = os.getenv "HOME"
+      local cwd = vim.fn.getcwd()
+      local exists_session = require("auto-session").session_exists_for_cwd()
+
+      if not exists_session and cwd == home then
+        vim.cmd "Telescope project"
+      end
+    end
+
     auto_session.setup {
       auto_restore_enabled = true,
       auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+      no_restore_cmds = { no_restore_hook },
     }
 
     local keymap = vim.keymap
