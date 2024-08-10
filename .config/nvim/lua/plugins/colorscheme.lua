70200111447a328462235502db20148c4247c4d0
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-08-10 15:54:55 +0900

    modify: add highlight settings

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -14,12 +14,13 @@ return {
       highlights = {
         LineNr = { fg = "#ABB2BF" },
         DiffChange = { bg = "#3B5469" },
-        ["@comment"] = { fg = "#8C95A8", fmt = "italic" },
       },
     }
-
     onedark.load()
 
-    vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#535965", bg = "NONE" })
+    local set_hl = vim.api.nvim_set_hl
+    set_hl(0, "WinSeparator", { fg = "#535965", bg = "NONE" })
+    set_hl(0, "@lsp.type.comment", { fg = "#8C95A8", italic = true })
+    set_hl(0, "@lsp.type.variable", { fg = "#EC818A", italic = true })
   end,
 }
