71834c5d6d58277b756f5b37fba793973c362ec8
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-08-13 10:56:33 +0900

    feature: add highlight group setting

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -17,10 +17,12 @@ return {
       },
     }
     onedark.load()
-
     local set_hl = vim.api.nvim_set_hl
     set_hl(0, "WinSeparator", { fg = "#535965", bg = "NONE" })
     set_hl(0, "@lsp.type.comment", { fg = "#8C95A8", italic = true })
+    set_hl(0, "@comment", { fg = "#8C95A8", italic = true })
     set_hl(0, "@lsp.type.variable", { fg = "#EC818A", italic = true })
+    set_hl(0, "DiagnosticUnnecessary", { fg = "#8A959D", strikethrough = true })
+    set_hl(0, "@variable", { fg = "#EC818A", italic = true })
   end,
 }
