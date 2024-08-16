02a812740675806f2da93c25b344c52d21cf6c5a
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-08-16 09:34:09 +0900

    fix: enable to convert multi-surrounding chars (like `SQL`, `HTML`, and so on)

--- .config/nvim/lua/core/libs.lua
+++ .config/nvim/lua/core/libs.lua
@@ -1,8 +1,24 @@
 ---Execute callback for each table item
 ---@param table table<any>
----@param callback fun(v: any, i: integer)
-_G.foreach = function(table, callback)
-  for i, v in ipairs(table) do
-    callback(v, i)
+---@param callback fun(value: any, index: integer)
+_G.iforeach = function(table, callback)
+  for index, value in ipairs(table) do
+    callback(value, index)
   end
+end
+
+---@param table table<any>
+---@param callback fun(value: any, key: string)
+_G.foreach = function(table, callback)
+  for key, value in pairs(table) do
+    callback(value, key)
+  end
+end
+
+--- Escape special characters in a string for use in pattern matching
+---@param str string
+---@return string
+_G.escape_pattern = function(str)
+  local escaped_str, _ = str:gsub("([%[%]%(%)%*%+%-%?%^%$])", "%%%1")
+  return escaped_str
 end
