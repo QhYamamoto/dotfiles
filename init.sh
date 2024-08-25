1eb36753a30b6fc767f9cee3b1d210c5bbc8f567
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-08-25 09:54:01 +0900

    feature: add scripts to install AutoHotkey

--- init.sh
+++ init.sh
@@ -182,10 +182,29 @@ curl -L -o install.js https://raw.githubusercontent.com/thqby/vscode-autohotkey2
 node install.js
 cd
 
+# AutoHotkey v1/v2
+ahk_dir="$WIN_HOME/.ahk"
+mkdir "$ahk_dir/App"
+curl -L -o ahk-v1.zip https://www.autohotkey.com/download/1.1/AutoHotkey_1.1.37.02.zip
+unzip ahk-v1.zip -d "$ahk_dir/App/v1"
+rm ahk-v1.zip
+curl -L -O https://www.autohotkey.com/download/ahk-v2.zip
+unzip ahk-v2.zip -d "$ahk_dir/App/v2"
+rm ahk-v2.zip
+
 # MouseGestureL
 curl -L -O https://ss1.xrea.com/pyonkichi.g1.xrea.com/archives/MGLahk140.zip
 unzip MGLahk140.zip -d "$ahk_dir/MouseGestureL"
 rm MGLahk140.zip
+
+# create Batch file that is to be placed to startup directory
+ahk_dir=${ahk_dir/"/mnt/c/"/"C:/"}
+startup_ahk_bat_path="$WIN_HOME/.ahk/startup_ahk.bat"
+touch "$startup_ahk_bat_path"
+echo "
+  start \"\" /B \"$ahk_dir/App/v2/AutoHotkey64.exe\" \"$ahk_dir/main.ahk\"
+  start \"\" /B \"$ahk_dir/App/v1/AutoHotkeyU64.exe\" \"$ahk_dir/MouseGestureL/MouseGestureL.ahk\"
+" >"$startup_ahk_bat_path"
 
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
