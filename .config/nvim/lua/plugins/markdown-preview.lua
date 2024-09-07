699812f19ba8a004ac90d8105d8050fe49f8f16f
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-09-07 11:09:13 +0900

    feature: markdown preview cmd

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -8,4 +8,9 @@ return {
   init = function()
     vim.g.mkdp_filetypes = { "markdown", "plantuml" }
   end,
+  config = function()
+    local keymap = vim.keymap
+
+    keymap.set("n", "<LEADER>mp", "<CMD>MarkdownPreview<CR>", { desc = "MarkdownPreview" })
+  end,
 }
