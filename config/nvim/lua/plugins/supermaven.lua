a0f1e2f7fc89dde8a7a90ff9d740397f1a02e198
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-10-08 14:32:25 +0900

    modify: settings for supermaven

--- config/nvim/lua/plugins/supermaven.lua
+++ config/nvim/lua/plugins/supermaven.lua
@@ -3,6 +3,14 @@ return {
   config = function()
     local supermaven = require "supermaven-nvim"
     supermaven.setup {
+      keymaps = {
+        accept_suggestion = "<C-j>",
+        accept_word = "<C-l>",
+      },
+      color = {
+        suggestion_color = "#545964",
+        cterm = 240,
+      },
       condition = function()
         return true
       end,
