4ed3ce615485d24c30e802949a7d4ee40048480f
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-10-08 14:38:51 +0900

    fix: lazygit installation

--- bash/packages/12_lazy_tui.sh
+++ bash/packages/12_lazy_tui.sh
@@ -5,6 +5,8 @@ LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/re
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
+rm lazygit.tar.gz
+rm -rf lazygit
 
 # lazydocker
 brew install jesseduffield/lazydocker/lazydocker
