c303984995ed8416af8ca04b60f0d012af13f328
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-10-08 14:37:41 +0900

    remove: zoxide cmd verification process (temporarily)

--- bash/packages/04_zoxide.sh
+++ bash/packages/04_zoxide.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-source ./bash/_functions.sh
-
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
-
-verify_package_installation "zoxide" "zoxide --version"
