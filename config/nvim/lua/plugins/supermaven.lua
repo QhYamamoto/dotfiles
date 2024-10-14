9ee6e93336da64258d743bdd2f02ff8a82c9a667
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-10-14 11:41:44 +0900

    modify: disable supermaven

--- config/nvim/lua/plugins/supermaven.lua
+++ config/nvim/lua/plugins/supermaven.lua
@@ -1,19 +1,19 @@
 return {
-  "supermaven-inc/supermaven-nvim",
-  config = function()
-    local supermaven = require "supermaven-nvim"
-    supermaven.setup {
-      keymaps = {
-        accept_suggestion = "<C-j>",
-        accept_word = "<C-l>",
-      },
-      color = {
-        suggestion_color = "#545964",
-        cterm = 240,
-      },
-      condition = function()
-        return true
-      end,
-    }
-  end,
+  -- "supermaven-inc/supermaven-nvim",
+  -- config = function()
+  --   local supermaven = require "supermaven-nvim"
+  --   supermaven.setup {
+  --     keymaps = {
+  --       accept_suggestion = "<C-j>",
+  --       accept_word = "<C-l>",
+  --     },
+  --     color = {
+  --       suggestion_color = "#545964",
+  --       cterm = 240,
+  --     },
+  --     condition = function()
+  --       return true
+  --     end,
+  --   }
+  -- end,
 }
