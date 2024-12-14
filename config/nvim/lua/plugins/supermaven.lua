88b8c121c6a5702e7e1cbfcee0f586e15cc7de94
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-12-14 23:00:59 +0900

    feat: restore supermaven plugin

--- config/nvim/lua/plugins/supermaven.lua
+++ config/nvim/lua/plugins/supermaven.lua
@@ -1,19 +1,30 @@
 return {
-  -- "supermaven-inc/supermaven-nvim",
-  -- config = function()
-  --   local supermaven = require "supermaven-nvim"
-  --   supermaven.setup {
-  --     keymaps = {
-  --       accept_suggestion = "<C-j>",
-  --       accept_word = "<C-l>",
-  --     },
-  --     color = {
-  --       suggestion_color = "#545964",
-  --       cterm = 240,
-  --     },
-  --     condition = function()
-  --       return true
-  --     end,
-  --   }
-  -- end,
+  "supermaven-inc/supermaven-nvim",
+  config = function()
+    local supermaven = require "supermaven-nvim"
+    supermaven.setup {
+      keymaps = {
+        accept_suggestion = "<C-j>",
+        accept_word = "<C-l>",
+      },
+      color = {
+        suggestion_color = "#545964",
+        cterm = 240,
+      },
+      condition = function()
+        return true
+      end,
+    }
+
+    local keymap = vim.keymap
+    local supermaven_api = require "supermaven-nvim.api"
+
+    keymap.set("n", "<LEADER>ms", function()
+      supermaven_api.start()
+    end, { silent = true, desc = "Start SuperMaven" })
+
+    keymap.set("n", "<LEADER>mS", function()
+      supermaven_api.stop()
+    end, { silent = true, desc = "Stop SuperMaven" })
+  end,
 }
