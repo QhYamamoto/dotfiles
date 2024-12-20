0929c8f30e89c5aaf21e48728df543d42d493b73
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-12-21 00:29:40 +0900

    fix: keymappings

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -3,22 +3,33 @@ vim.o.mouse = "a"
 
 local keymap = vim.keymap
 
--- function for repeat function
-local function repeat_func(func, count)
-  if count == 0 then
-    func()
-    return
-  end
-  for _ = 1, count do
-    func()
-  end
-end
-
--- general
 keymap.set({ "i", "c" }, "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 keymap.set({ "i", "c" }, "ｊｋ", "<ESC>", { desc = "Exit insert mode with ｊｋ" })
 keymap.set("n", "い", "i", { desc = "Enter insert mode with い" })
 keymap.set("i", "<C-v>", "<ESC>pa", { desc = "Paste." })
+keymap.set({ "n", "v" }, "d", '"_d', { desc = "Yank on delete" })
+keymap.set("n", "<C-c>", "<C-w>w", { noremap = true, silent = true, desc = "Jump to floating window" })
+keymap.set("n", "<M-f>", "w", { noremap = true, silent = true, desc = "Move to next word" })
+keymap.set({ "n", "i" }, "<C-s>", "<CMD>silent! w<CR>", { desc = "Store" })
+keymap.set({ "n", "i", "v" }, "<C-z>", "<CMD>silent! u<CR>", { desc = "Undo" })
+keymap.set({ "n", "i", "v" }, "<C-y>", "<CMD>silent! redo<CR>", { desc = "Redo" })
+keymap.set({ "n", "i", "v" }, "<C-w>", "<CMD>bd!<CR>", { desc = "Delete current buffer" })
+keymap.set("n", "<S-Left>", "<C-W><", { noremap = true, silent = true, desc = "Expand window to the left" })
+keymap.set("n", "<S-Right>", "<C-W>>", { noremap = true, silent = true, desc = "Expand window to the right" })
+keymap.set("n", "<S-Up>", "<C-W>-", { noremap = true, silent = true, desc = "Expand window to top" })
+keymap.set("n", "<S-Down>", "<C-W>+", { noremap = true, silent = true, desc = "Expand window to bottom" })
+keymap.set("n", "<tab>", "<CMD>><CR>", { desc = "Incriment indent level" })
+keymap.set("n", "<S-Tab>", "<CMD><<CR>", { desc = "Decriment indent level" })
+keymap.set("n", "<A-Down>", '"xdd"xp<CMD>Format<CR>', { desc = "Swap current line with line below" })
+keymap.set("i", "<A-Down>", '<ESC>"xdd"xpi<CMD>Format<CR>', { desc = "Swap current line with line below" })
+keymap.set({ "n", "i" }, "<A-Up>", '<Up>"xdd"xp<Up><CMD>Format<CR>', { desc = "Swap current line with line above" })
+keymap.set("i", "<A-Up>", '<ESC><Up>"xdd"xp<Up>i<CMD>Format<CR>', { desc = "Swap current line with line above" })
+keymap.set("n", "<C-a>", "gg^vG$", { desc = "Select whole text" })
+keymap.set("n", "<C-e>", "$", { noremap = true, silent = true, desc = "Move cursor to the end of the line" })
+keymap.set("i", "<C-e>", "<ESC>$a", { noremap = true, silent = true, desc = "Move cursor to the end of the line" })
+keymap.set("t", "jk", "<C-\\><C-n>", { noremap = true, silent = true, desc = "Focus out from the terminal" })
+keymap.set({ "n", "v" }, "U", "<C-a>", { noremap = true, silent = true, desc = "Increment number" })
+keymap.set({ "n", "v" }, "D", "<C-x>", { noremap = true, silent = true, desc = "Increment number" })
 keymap.set("n", "<ESC>", function()
   -- if search register is not nil, then execute nohl command
   if vim.fn.getreg "/" ~= "" then
@@ -43,66 +54,46 @@ keymap.set("n", "<ESC>", function()
 
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<ESC>", true, false, true), "n", true)
 end)
-keymap.set({ "n", "v" }, "d", '"_d') -- prevent to yank on delete
-keymap.set("n", "<C-c>", "<C-w>w", { noremap = true, silent = true }) -- jump to floating window
-keymap.set("n", "<LEADER>q", "<CMD>qa<CR>")
-keymap.set("n", "<M-u>", "<C-a>", { noremap = true, silent = true })
-keymap.set("n", "<M-d>", "<C-x>", { noremap = true, silent = true })
-keymap.set("n", "<M-f>", "w", { noremap = true, silent = true })
+keymap.set("n", "J", function()
+  local concatenateNextLintToCurrentLine = function()
+    local current_line = vim.api.nvim_get_current_line()
+    local current_line_number = vim.fn.line "."
+    local next_line = vim.api.nvim_buf_get_lines(0, current_line_number, current_line_number + 1, false)[1]
+    if next_line then
+      local next_line_trimmed = next_line:gsub("^%s+", "")
+      vim.api.nvim_set_current_line(current_line .. next_line_trimmed)
+      vim.api.nvim_buf_set_lines(0, current_line_number, current_line_number + 1, false, {})
+    end
+  end
 
--- window
+  local count = vim.v.count
+  if count == 0 then
+    count = 1
+  end
+
+  for _ = 1, count do
+    concatenateNextLintToCurrentLine()
+  end
+end, { noremap = true, silent = true, desc = "Concatenate next lint to current line" })
+
+keymap.set("n", "<LEADER>q", "<CMD>qa<CR>", { desc = "Quit neovim" })
 keymap.set("n", "<LEADER>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<LEADER>sh", "<C-w>s", { desc = "Split window horizontally" })
 keymap.set("n", "<LEADER>sx", "<CMD>close<CR>", { desc = "Close current split" })
 keymap.set("n", "<LEADER>se", "<C-w>=", { desc = "Make splits equal size" })
-keymap.set("n", "<S-Left>", "<C-W><", { noremap = true, silent = true })
-keymap.set("n", "<S-Right>", "<C-W>>", { noremap = true, silent = true })
-keymap.set("n", "<S-Up>", "<C-W>-", { noremap = true, silent = true })
-keymap.set("n", "<S-Down>", "<C-W>+", { noremap = true, silent = true })
-
--- shortcuts to ensure compatibility with other editors
 keymap.set("n", "<LEADER>bp", "<CMD>bprev<CR>", { desc = "Jump to previous buffer" })
-keymap.set({ "n", "i" }, "<C-s>", "<CMD>silent! w<CR>", { desc = "Store" })
-keymap.set({ "n", "i", "v" }, "<C-z>", "<CMD>silent! u<CR>", { desc = "Undo" })
-keymap.set({ "n", "i", "v" }, "<C-y>", "<CMD>silent! redo<CR>", { desc = "Redo" })
-keymap.set({ "n", "i", "v" }, "<C-w>", "<CMD>bd!<CR>", { desc = "Delete current buffer" })
-keymap.set("n", "<tab>", "<CMD>><CR>", { desc = "Incriment indent level" })
-keymap.set("n", "<S-Tab>", "<CMD><<CR>", { desc = "Decriment indent level" })
-keymap.set("n", "<A-Down>", '"xdd"xp<CMD>Format<CR>', { desc = "Swap current line with line below" })
-keymap.set("i", "<A-Down>", '<ESC>"xdd"xpi<CMD>Format<CR>', { desc = "Swap current line with line below" })
-keymap.set({ "n", "i" }, "<A-Up>", '<Up>"xdd"xp<Up><CMD>Format<CR>', { desc = "Swap current line with line above" })
-keymap.set("i", "<A-Up>", '<ESC><Up>"xdd"xp<Up>i<CMD>Format<CR>', { desc = "Swap current line with line above" })
-keymap.set("n", "<C-a>", "gg^vG$", { desc = "Select whole text" })
-keymap.set("n", "<C-e>", "$", { noremap = true, silent = true })
-keymap.set("i", "<C-e>", "<ESC>$a", { noremap = true, silent = true })
-
--- for terminal
-keymap.set("t", "jk", "<C-\\><C-n>", { noremap = true })
-
-keymap.set({ "n", "v" }, "U", "<C-a>", { noremap = true, silent = true, desc = "Increment number" })
-keymap.set({ "n", "v" }, "D", "<C-x>", { noremap = true, silent = true, desc = "Increment number" })
-
 keymap.set("n", "<LEADER>yf", function()
   vim.fn.setreg("+", vim.fn.expand "%:p")
   print "Filepath has been copied to your clipboard!!"
-end, { desc = "save fullpath to currently opened file in a buffer", noremap = true, silent = true })
-
+end, { noremap = true, silent = true, desc = "Save fullpath to currently opened file in a buffer" })
 keymap.set("n", "<LEADER>yb", function()
   vim.fn.setreg("+", vim.fn.expand "%:t")
   print "File basename has been copied to your clipboard!!"
-end, { desc = "save basename of currently opened file in a buffer", noremap = true, silent = true })
-
-keymap.set("n", "J", function()
-  repeat_func(function()
-    local current_line = vim.api.nvim_get_current_line()
-    local next_line = vim.api.nvim_buf_get_lines(0, vim.fn.line "." + 1 - 1, vim.fn.line "." + 2 - 1, false)[1]
-    if next_line then
-      local next_line_trimmed = next_line:gsub("^%s+", "")
-      vim.api.nvim_set_current_line(current_line .. next_line_trimmed)
-      vim.api.nvim_buf_set_lines(0, vim.fn.line "." + 1 - 1, vim.fn.line "." + 1, false, {})
-    end
-  end, vim.v.count or 1)
-end, { noremap = true, silent = true })
+end, { noremap = true, silent = true, desc = "Save basename of currently opened file in a buffer" })
+keymap.set("n", "<LEADER>yn", function()
+  vim.fn.setreg("+", vim.fn.expand "%:t:r")
+  print "File basename without extension has been copied to your clipboard!!"
+end, { noremap = true, silent = true, desc = "Copy basename without extension of currently opened file" })
 
 --------------------------------------------------
 -- convert surrounding chars command
