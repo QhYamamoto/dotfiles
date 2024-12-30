adf28a8d7fa7f97d73202df96443128602b53eb9
QhYamamoto <kazuma.yamamoto819@gmail.com>
2024-12-30 09:25:29 +0900

    feat: make symlink from windows path -> wsl path

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,18 +1,20 @@
-use std::{collections::HashMap, env};
+use std::{collections::HashMap, process::Command};
 
-use my_cli_tool::modules::filesystem;
+use my_cli_tool::modules::filesystem::{self, get_wsl_home};
 
 const DIRECTORIES_TO_CREATE: [&str; 2] = ["/.config", "/.config/broot"];
 const DOTFILES_DIR: &str = "dotfiles";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let home = env::var("HOME").expect("Error: Environment Variable `HOME` is not set.");
+    let wsl_home = get_wsl_home().expect("Wsl home is Empty!!");
 
+    // Make necessary directories.
     for dir in DIRECTORIES_TO_CREATE {
-        let full_path = home.clone() + dir;
+        let full_path = wsl_home.clone() + dir;
         filesystem::create_dir_if_not_exists(&full_path)?;
     }
 
+    // Make symlinks (from linux path to linux path).
     let symlink_hash_map: HashMap<&str, Vec<&str>> = HashMap::from([
         (
             "", // Prefix must start without `/`!!
@@ -33,12 +35,39 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     for (prefix, sources) in symlink_hash_map {
         for source in sources {
-            let dest = format!("{}/.{}{}", home, prefix, source);
-            let src = format!("{}/{}/home/{}{}", home, DOTFILES_DIR, prefix, source);
+            let dest = format!("{}/.{}{}", wsl_home, prefix, source);
+            let src = format!("{}/{}/home/{}{}", wsl_home, DOTFILES_DIR, prefix, source);
 
             println!("Try to create symlink: {} -> {}", src, dest);
             filesystem::create_symlink(&src, &dest)?;
         }
+    }
+
+    // Make symlinks from windows wezterm path to wsl wezterm path.
+    if let Some(win_home) = filesystem::get_win_home() {
+        let wsl_home_in_windows_fs_format = filesystem::get_wsl_home_in_windows_fs_format()
+            .expect("Failed to get wsl home directory in windows file system format.");
+
+        let win_wezterm_dir = format!("{}\\.config\\wezterm", win_home);
+        let wsl_wezterm_dir = format!(
+            "{}\\{}\\home\\config\\wezterm",
+            wsl_home_in_windows_fs_format, DOTFILES_DIR
+        );
+        let mklink_ps1_path = format!("{}/{}/powershell/mklink.ps1", wsl_home, DOTFILES_DIR);
+
+        Command::new("powershell.exe")
+            .args([
+                "-ExecutionPolicy",
+                "Bypass",
+                "-File",
+                &mklink_ps1_path,
+                "-LinkPath",
+                &win_wezterm_dir,
+                "-TargetPath",
+                &wsl_wezterm_dir,
+            ])
+            .status()
+            .expect("Failed to execute Powershell script.");
     }
 
     Ok(())
