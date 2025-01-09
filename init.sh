b431b99bdca061c09d1cc3c4d202e982b8780e6a
QhYamamoto <kazuma.yamamoto819@gmail.com>
2025-01-09 22:56:42 +0900

    fix: init.sh

--- init.sh
+++ init.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-scripts_dir="./bash"
-
-# Get the current user's username
-USER_NAME=$(whoami)
-
-# Create a backup of the sudoers file
-sudo cp /etc/sudoers /etc/sudoers.bak
-
-# Add the current user to the sudoers file
-echo "$USER_NAME ALL=(ALL) NOPASSWD:ALL" | sudo EDITOR='tee -a' visudo
-
-dotfiles init
+$HOME/dotfiles/command/dotfiles init
 
 # If WIN_HOME is not null,
 WIN_HOME=$(wslpath "$(cmd.exe /C 'echo %HOMEDRIVE%%HOMEPATH%' 2>/dev/null | tr -d '\r')")
 if [[ -n "$WIN_HOME" ]]; then
-  dotfiles ahk
+  $HOME/dotfiles/command/dotfiles ahk
 fi
 
 # Set Zsh as default shell
@@ -24,7 +13,3 @@ echo "Setting Zsh as default shell. Please enter your password..."
 chsh -s $(which zsh)
 
 echo "If authentication fails, please run the following command manually to set Zsh as your default shell: \`chsh -s \$(which zsh)\`."
-
-# restore original sudoers file
-sudo cp /etc/sudoers.bak /etc/sudoers
-sudo chmod 0440 /etc/sudoers
