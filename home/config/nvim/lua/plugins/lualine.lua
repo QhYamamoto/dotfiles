6396adb39a44e1b642fe82bdf1b01ce3a5ef8e60
QhYamamoto <kazuma.yamamoto819@gmail.com>
2025-01-11 11:22:56 +0900

    modify: lualine layout

--- home/config/nvim/lua/plugins/lualine.lua
+++ home/config/nvim/lua/plugins/lualine.lua
@@ -9,39 +9,51 @@ return {
     local lazy_status = require "lazy.status" -- to configure lazy pending updates count
     local transparent = require "transparent"
 
-    local function command_display()
-      if vim.fn.reg_recording() ~= "" then
-        return "Recording @" .. vim.fn.reg_recording()
-      else
-        return ""
-      end
-    end
-
     transparent.clear_prefix "lualine"
 
     -- configure lualine with modified theme
     lualine.setup {
       options = {
         theme = "onedark",
+        section_separators = "",
+        component_separators = "",
       },
       sections = {
-        lualine_c = { command_display },
-        lualine_x = {
+        lualine_a = {
+          {
+            "mode",
+            padding = { left = 1, right = 0 },
+          },
+          {
+            function()
+              local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+              return string.format(" @ L:%d Col:%d", row, col)
+            end,
+            color = function()
+              local themes = require "lualine.themes.auto"
+              local mode = vim.fn.mode()
+              return themes[mode] or themes.normal
+            end,
+            padding = { left = 0, right = 1 },
+          },
+        },
+        lualine_c = {
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
             color = { fg = "#ff9e64" },
           },
           { "encoding" },
-          { "fileformat" },
           { "filetype" },
+          function() -- Show current recording register.
+            if vim.fn.reg_recording() ~= "" then
+              return "Recording @" .. vim.fn.reg_recording()
+            else
+              return ""
+            end
+          end,
         },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
+        lualine_x = {},
         lualine_y = {},
         lualine_z = {},
       },
