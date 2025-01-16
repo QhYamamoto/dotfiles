0beca64a650fc590da580fcbb4c0c914123dd220
QhYamamoto <kazuma.yamamoto819@gmail.com>
2025-01-16 14:08:44 +0900

    modify: mklink.ps1
    Delete existing directory/symbolic link before creating.

--- powershell/mklink.ps1
+++ powershell/mklink.ps1
@@ -16,5 +16,19 @@ Write-Host "Creating symlink from $LinkPath to $TargetPath"
 Write-Host "LinkPath: $LinkPath"
 Write-Host "TargetPath: $TargetPath"
 
+# Check if the symlink already exists and delete it if necessary
+if (Test-Path -Path $LinkPath)
+{
+  if ((Get-Item $LinkPath).Attributes -band [System.IO.FileAttributes]::ReparsePoint)
+  {
+    Write-Host "Existing symlink found at $LinkPath. Deleting..."
+    Remove-Item -Path $LinkPath -Force
+  } else
+  {
+    Write-Host "A non-symlink file or directory exists at $LinkPath. Deleting..."
+    Remove-Item -Path $LinkPath -Recurse -Force
+  }
+}
+
 # Use mklink with cmd.exe
 cmd.exe /c mklink "$LinkPath" "$TargetPath"
