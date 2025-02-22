514dd2264c8698ca3387b3bc997d661c6cd4e499
QhYamamoto <kazuma.yamamoto819@gmail.com>
2025-02-22 22:32:07 +0900

    modify: in-and-out
    change keymapping

--- home/config/nvim/lua/plugins/in-and-out.lua
+++ home/config/nvim/lua/plugins/in-and-out.lua
@@ -1,7 +1,7 @@
 return {
   "ysmb-wtsg/in-and-out.nvim",
   config = function()
-    vim.keymap.set("i", "<C-CR>", function()
+    vim.keymap.set("n", "<C-l>", function()
       require("in-and-out").in_and_out()
     end)
   end,
