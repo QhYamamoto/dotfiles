15822041f02f0b3d5bbfbba0f23e433f3c018f1c
QhYamamoto <kazuma.yamamoto819@gmail.com>
2025-02-22 23:05:44 +0900

    modify: dial.nvim
    extend augends default group

--- home/config/nvim/lua/plugins/dial.lua
+++ home/config/nvim/lua/plugins/dial.lua
@@ -1,14 +1,44 @@
 return {
   "monaqa/dial.nvim",
   config = function()
-    local dial = require "dial.map"
+    local augend = require "dial.augend"
+    local config = require "dial.config"
+
+    config.augends:register_group {
+      default = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.constant.alias.bool,
+        augend.constant.alias.alpha,
+        augend.constant.alias.Alpha,
+        augend.date.alias["%Y/%m/%d"],
+        augend.date.alias["%m/%d/%Y"],
+        augend.date.alias["%d/%m/%Y"],
+        augend.date.alias["%m/%d/%y"],
+        augend.date.alias["%d/%m/%y"],
+        augend.date.alias["%m/%d"],
+        augend.date.alias["%-m/%-d"],
+        augend.date.alias["%Y-%m-%d"],
+        augend.date.alias["%d.%m.%Y"],
+        augend.date.alias["%d.%m.%y"],
+        augend.date.alias["%d.%m."],
+        augend.date.alias["%-d.%-m."],
+        augend.date.alias["%Y年%-m月%-d日"],
+        augend.date.alias["%Y年%-m月%-d日(%ja)"],
+        augend.date.alias["%H:%M:%S"],
+        augend.date.alias["%H:%M"],
+        augend.semver.alias.semver,
+      },
+    }
+
+    local map = require "dial.map"
     local keymap = vim.keymap
 
-    keymap.set("n", "U", dial.inc_normal(), { noremap = true, silent = true, desc = "Increment by dial" })
-    keymap.set("v", "U", dial.inc_visual(), { noremap = true, silent = true, desc = "Increment by dial" })
-    keymap.set("n", "D", dial.dec_normal(), { noremap = true, silent = true, desc = "Decrement by dial" })
-    keymap.set("v", "D", dial.dec_normal(), { noremap = true, silent = true, desc = "Decrement by dial" })
-    keymap.set("n", "gU", dial.inc_gnormal(), { noremap = true, silent = true, desc = "g incriment by dial" })
-    keymap.set("n", "gD", dial.dec_gnormal(), { noremap = true, silent = true, desc = "g incriment by dial" })
+    keymap.set("n", "U", map.inc_normal(), { noremap = true, silent = true, desc = "Increment by dial" })
+    keymap.set("v", "U", map.inc_visual(), { noremap = true, silent = true, desc = "Increment by dial" })
+    keymap.set("n", "D", map.dec_normal(), { noremap = true, silent = true, desc = "Decrement by dial" })
+    keymap.set("v", "D", map.dec_normal(), { noremap = true, silent = true, desc = "Decrement by dial" })
+    keymap.set("n", "gU", map.inc_gnormal(), { noremap = true, silent = true, desc = "g incriment by dial" })
+    keymap.set("n", "gD", map.dec_gnormal(), { noremap = true, silent = true, desc = "g incriment by dial" })
   end,
 }
