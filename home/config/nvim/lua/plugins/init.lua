692b646b377881938e9ed0845d4eee2b1497542a
QhYamamoto <kazuma.yamamoto819@gmail.com>
2026-06-07 00:18:16 +0900

    modify: lazy load tmux navigator

--- home/config/nvim/lua/plugins/init.lua
+++ home/config/nvim/lua/plugins/init.lua
@@ -1,7 +1,57 @@
 return {
   "rhysd/conflict-marker.vim",
   "nvim-lua/plenary.nvim",
-  "christoomey/vim-tmux-navigator",
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
+    keys = {
+      {
+        "<C-h>",
+        function()
+          vim.cmd.TmuxNavigateLeft()
+        end,
+        mode = "n",
+      },
+      {
+        "<C-j>",
+        function()
+          vim.cmd.TmuxNavigateDown()
+        end,
+        mode = "n",
+      },
+      {
+        "<C-k>",
+        function()
+          vim.cmd.TmuxNavigateUp()
+        end,
+        mode = "n",
+      },
+      {
+        "<C-l>",
+        function()
+          vim.cmd.TmuxNavigateRight()
+        end,
+        mode = "n",
+      },
+      {
+        "<C-\\>",
+        function()
+          vim.cmd.TmuxNavigatePrevious()
+        end,
+        mode = "n",
+      },
+    },
+  },
   "mcauley-penney/tidy.nvim",
   "HiPhish/rainbow-delimiters.nvim",
   "andres-lowrie/vim-sqlx",
